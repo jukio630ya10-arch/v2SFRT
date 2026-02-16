2cef87e42760e3943faae2a44342cb83e1949336
julioSosa <jukio630ya10@gmail.com>
2026-02-15 23:38:28 -0500

    mensaje

--- src/main/java/com/cibertec/EFSRTII_proyecto/controller/ProductoController.java
+++ src/main/java/com/cibertec/EFSRTII_proyecto/controller/ProductoController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cibertec.EFSRTII_proyecto.model.Categoria;
 import com.cibertec.EFSRTII_proyecto.model.Producto;
 import com.cibertec.EFSRTII_proyecto.service.CategoriaService;
 import com.cibertec.EFSRTII_proyecto.service.ProductoService;
@@ -32,13 +34,16 @@ public class ProductoController {
     
     @GetMapping("/nuevo")
     public String nuevo(Model model) {
-        model.addAttribute("producto", new Producto());
-        model.addAttribute("categorias", categoriaService.listar());
+    	 Producto producto = new Producto();
+         producto.setCategoria(new Categoria());
+         model.addAttribute("producto", producto);
+         model.addAttribute("categorias", categoriaService.listar());
         return "productos/form";
     }
 
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Producto producto) {
+    public String guardar(@ModelAttribute Producto producto, @RequestParam("categoria.id") Long categoriaId) {
+        producto.setCategoria(categoriaService.obtenerPorId(categoriaId));
         service.guardar(producto);
         return "redirect:/productos";
     }
